c7617f0c0d994e5c1962aa0c32bebc10a1a7e09e
Elvis Lee <elvis2681@gmail.com>
2021-05-28 18:10:49 -0700

    technical error

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 const apiRoutes = require("./api");
-const homeRoutes = require("./homeRoutes");
+const homeroutes = require("./homeroutes");
 
-router.use("/", homeRoutes);
+router.use("/", homeroutes);
 router.use("/api", apiRoutes);
 
 module.exports = router;
